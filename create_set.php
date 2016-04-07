6b743aba3a7c1b7bcfd53b09d60b01f2e4f74318
yan vologzhanin <yanvolo@gmail.com>
2016-04-06 21:27:40 -0400

    04/06/2016

--- create_set.php
+++ create_set.php
@@ -1,27 +1,19 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html>
 	<head xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-		<!-- Latest compiled and minified CSS -->
-		<link rel="stylesheet" href="css/bootstrap.min.css"> </link>
-
-		<!-- Optional theme -->
-		<link rel="stylesheet" href="css/bootstrap-theme.min.css"> </link>
-		  
-		<meta name="google-signin-scope" content="profile email"/>
-		<meta name="google-signin-client_id" content="1054699344422-jr8acquecheeh5lrghtcvhabto42hni4.apps.googleusercontent.com"/>
-	   <script src="https://apis.google.com/js/platform.js" async defer></script>
-		<style>
-		* {
-			border-radius: 0 !important;
-		}
-      .strokeme{
-            color: white;
-            text-shadow:
-            -2px -2px 0 #000,
-            2px -2px 0 #000,
-            -2px 2px 0 #000,
-            2px 2px 0 #000;  
-        }    
+     <!-- Latest compiled and minified CSS -->
+	 <link rel="stylesheet" href="css/bootstrap.min.css"></link>
+	 <!-- Optional theme -->
+	 <link rel="stylesheet" href="css/bootstrap-theme.min.css"></link>
+	 <!-- Personal Deafult Theme-->
+	 <link rel="stylesheet" href="css/default.css"></link>
+	 
+	 <meta name="google-signin-scope" content="profile email"/>
+     <meta name="google-signin-client_id" content="1054699344422-jr8acquecheeh5lrghtcvhabto42hni4.apps.googleusercontent.com"/>
+	 <script src="https://apis.google.com/js/platform.js" async defer></script>
+	 <link rel="icon" type="img/ico" href="cp.ico">
+     <title>Chalk it up!</title>
+		<style>  
       textarea { 
           resize:none;
       }
@@ -30,10 +22,17 @@
       }
       .answer{
           placeholder="Answer";
+		  padding:5px;
+		  margin:10px;
+		  width:100%;
+		  height:1.5em;
       }
       .distractor{
           placeholder="First Distractor";
       }
+	  .card{
+		  width:100%;
+	  }
 	  .card-table{
 		  padding:0px;
 	  }
@@ -46,43 +45,27 @@
 	  .warning{
 		  border: 1px solid red;
 	  }
-	  
-
-		</style>
-		<title>Chalk it up!</title>
-	
+	</style>
 	</head>
 	<body>
 		<?php require 'library.php'; printNav(); ?>
+		
 		<form>
+		<h3> Create a set! </h3>
+		<p> Helper text that explains stuff </p>
+		<div class="form-group">
+			<label> Set Name: </label>
+			<input type="text" placeholder="Set Name" name="set-name" class="form-control card-input" id="set-name">
+        </div>
+		
 		<table class="table table-bordered card-table">
          <thead class="thead-inverse">
              <th> <h4 class="text-center"> Question </h4> </th>
              <th> <h4 class="text-center"> Answer </h4> </th>
              <th> <h4 class="text-center"> Distractors </h4> </th>
          </thead>
-         <tbody class="text-center" id="deck">
-             <tr id="card-1"> 
-                 <td> <textarea rows="4" cols="50" class="question form-control card-input" id="qustion-1"></textarea> </td>
-                 <td> <textarea rows="4" cols="50" class="answer form-control card-input" id="answer-1"></textarea> </td>
-                 <td> 
-					<div class="form-group">
-                      <textarea rows="1" cols="50" class="distractor form-control card-input" id="distractor-1a"></textarea> <br> 
-                      <textarea rows="1" cols="50" class="distractor form-control card-input" id="distractor-1b"></textarea> <br> 
-                      <textarea rows="1" cols="50" class="distractor form-control card-input" id="distractor-1c"></textarea>
-					</div>  
-                 </td>
-             </tr>
-            <tr id="card-2"> 
-                 <td> <textarea rows="4" cols="50" class="question form-control card-input" id="qustion-2"></textarea> </td>
-                 <td> <textarea rows="4" cols="50" class="answer form-control card-input" id="answer-2"></textarea> </td>
-                 <td> 
-                     <textarea rows="1" cols="50" class="distractor form-control card-input" id="distractor-2a"></textarea> <br> 
-                      <textarea rows="1" cols="50" class="distractor form-control card-input" id="distractor-2b"></textarea> <br> 
-                      <textarea rows="1" cols="50" class="distractor form-control card-input" id="distractor-2c"></textarea>
-                 </td>
-             </tr>
-         </tbody>
+		 <!-- 5 cards inserted by Jquerry at start-->
+         <tbody class="text-center" id="deck"></tbody>
       </table>
 
       
@@ -98,17 +81,17 @@
       <!-- Latest compiled and minified JavaScript -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"   integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
       <script type="text/javascript">
-          numCards = 2;
+          numCards = 0;
           function createID(name){
             return "#" + name.toString();   
           }
           function generateCard(number){
-            var ret = '<tr id="card-' + number.toString() + '">';
-            ret += '<td> <textarea rows="4" cols="50" class="question card-input form-control" id="qustion-'+number.toString()+'"></textarea> </td>';
-            ret += '<td> <textarea rows="4" cols="50" class="answer card-input form-control" id="answer-'+number.toString()+'"></textarea> </td> <td>';
-            ret += '<textarea rows="1" cols="50" class="distractor card-input form-control" id="distractor-'+number.toString()+'a"></textarea> <br>';
-            ret += '<textarea rows="1" cols="50" class="distractor card-input form-control" id="distractor-'+number.toString()+'b"></textarea> <br>';
-            ret += '<textarea rows="1" cols="50" class="distractor card-input form-control" id="distractor-'+number.toString()+'c"></textarea>';
+            var ret = '<tr class="card" id="card-' + number.toString() + '">';
+            ret += '<td> <textarea rows="4" cols="50" class="question form-control card-input" id="question-'+number.toString()+'"></textarea> </td>';
+            ret += '<td> <textarea rows="4" cols="50" class="answer form-control card-input" id="answer-'+number.toString()+'"></textarea> </td> <td>';
+            ret += '<textarea rows="1" cols="50" class="distractor form-control card-input" id="distractor-'+number.toString()+'a"></textarea> <br>';
+            ret += '<textarea rows="1" cols="50" class="distractor form-control card-input" id="distractor-'+number.toString()+'b"></textarea> <br>';
+            ret += '<textarea rows="1" cols="50" class="distractor form-control card-input" id="distractor-'+number.toString()+'c"></textarea>';
             ret += '</td></tr>'
             return ret;
           }
@@ -116,12 +99,20 @@
 			  numCards++;
 			  //Create New Blank Card
 			  $("#deck").append(generateCard(numCards));
+			  $('.card-input').blur(function(){
+					if(isBlank($(this))){
+						$(this).addClass('warning');
+					}
+					else{
+						$(this).removeClass('warning');
+					}
+				});
           }
           function removeCard(){
               if(numCards>1){
-                      $(createID("card-"+numCards)).empty();
-                      $(createID("card-"+numCards)).remove();
-                      numCards--;
+				  $(createID("card-"+numCards)).empty();
+				  $(createID("card-"+numCards)).remove();
+				  numCards--;
                 }
           }
 		  function isBlank($field){
@@ -134,40 +125,69 @@
 			   
 		  }
 		  function checkForBlanks(){
+			  $('.card-input').blur();
+			  if(isBlank($("#set-name"))){
+				  alert("Please name your set");
+				  return false;
+			  }
 			  for(i=1;i<=numCards;i++){
-				  console.log(i);
 				  $questionID = $("#question-"+ i.toString());
 				  $answerID = $("#answer-"+ i.toString());
 				  $distractorAID = $("#distractor-"+ i.toString() + "a");
 				  $distractorBID = $("#distractor-"+ i.toString() + "b");
 				  $distractorCID = $("#distractor-"+ i.toString() + "c");
-				  if(isBlank($questionID)||isBlank($answerID) ||isBlank($distractorAID) ||isBlank($distractorBID) ||isBlank($distractorCID)){
-					  $('.card-input').blur();
+				  
+				  questionCheck = isBlank($questionID);
+				  answerCheck = (isBlank($answerID));
+				  distractorACheck = isBlank($distractorAID);
+				  distractorBCheck = isBlank($distractorBID);
+				  distractorCCheck = isBlank($distractorCID);
+				  
+				  if((isBlank($questionID)||isBlank($answerID) ||isBlank($distractorAID) ||isBlank($distractorBID) ||isBlank($distractorCID))
+					  && !(isBlank($questionID)&&isBlank($answerID) &&isBlank($distractorAID) &&isBlank($distractorBID) &&isBlank($distractorCID))){
 					  return confirm("Some fields are blank. Are you sure you would like to submit blank entries?"); 
 				  }
 			  }
 			  return true; 
 		  }
 		  function submitCard(){
-			  return checkForBlanks();
+			  deck = [];
+			  if(checkForBlanks()){
+				  
+				  for(i=1;i<=numCards;i++){
+					  $questionID = $("#question-"+ i.toString());
+					  $answerID = $("#answer-"+ i.toString());
+					  $distractorAID = $("#distractor-"+ i.toString() + "a");
+					  $distractorBID = $("#distractor-"+ i.toString() + "b");
+					  $distractorCID = $("#distractor-"+ i.toString() + "c");
+					  deck.push({
+						  question:$questionID.val(),
+						  answer:$answerID.val(),
+						  distractorA:$distractorAID.val(),
+						  distractorB:$distractorBID.val(),
+						  distractorC:$distractorCID.val()
+						  
+					  });
+				  }
+			  }
+			  return deck;
 			  
 		  }
-          //Listeners
+          //Listeners and init
           $(document).ready(function(){
+			  //init
+			  //Add five cards for init
+			  for(i=0;i<5;i++){
+				 addCard(); 
+			  }
+			
               $("#add-card").click(function(){
                   addCard();
               });
               $("#rem-card").click(function(){
                   removeCard();
               });
-			  $('.card-input').blur(function(){
-					if(isBlank($(this))){
-						$(this).addClass('warning');
-					}
-					else{
-						$(this).removeClass('warning');
-					}
-				});
+			  
 				$('#create-set').click(function(){
 					submitCard();
 				});
--- index.php
+++ index.php
@@ -1,46 +1,41 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html>
   <head xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-    <!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="css/bootstrap.min.css"> </link>
-
-<!-- Optional theme -->
-<link rel="stylesheet" href="css/bootstrap-theme.min.css"> </link>
-      
-	  <meta name="google-signin-scope" content="profile email"/>
-        <meta name="google-signin-client_id" content="1054699344422-jr8acquecheeh5lrghtcvhabto42hni4.apps.googleusercontent.com"/>
-	   <script src="https://apis.google.com/js/platform.js" async defer></script>
-	   <link rel="icon" type="img/ico" href="cp.ico">
-    <style>
-	* {
-		border-radius: 0 !important;
-	}
-	#blackboard{
+     <!-- Latest compiled and minified CSS -->
+	 <link rel="stylesheet" href="css/bootstrap.min.css"></link>
+	 <!-- Optional theme -->
+	 <link rel="stylesheet" href="css/bootstrap-theme.min.css"></link>
+	 <!-- Personal Deafult Theme-->
+	 <link rel="stylesheet" href="css/default.css"></link>
+	 
+	 <meta name="google-signin-scope" content="profile email"/>
+     <meta name="google-signin-client_id" content="1054699344422-jr8acquecheeh5lrghtcvhabto42hni4.apps.googleusercontent.com"/>
+	 <script src="https://apis.google.com/js/platform.js" async defer></script>
+	 <link rel="icon" type="img/ico" href="cp.ico">
+     <title>Chalk it up!</title>
+	 <style>
+	 #blackboard{
 		background: url("https://www.googledrive.com/host/0B9YkvbHM062yZTJzdGtGUi1jelk/img/BB.png") no-repeat center center fixed; 
 		background-size: cover;
 	}
 	.big-image{
-		height: 500px;
+		width: 100%;
 		background-color:black;
 	}
 	</style>
-      
-    <title>Chalk it up!</title>
-	
   </head>
   <body>
-		<?php 
+	<?php 
 	  require "library.php"; 
 	  printNav();
-  ?>
-	 
-      <div class="jumbotron text-center" id="blackboard">
-			<div class="container">
-				<h1 style='color:white'> Welcome to Chalk up! </h1>
-				<p style='color:white'>  Fun Learning throughout the day! </p>
-				
-			</div>
+	 ?>
+	<div class="jumbotron text-center" id="blackboard">
+		<div class="container">
+			<h1 style='color:white'> Welcome to Chalk up! </h1>
+			<p style='color:white'>  Fun Learning throughout the day! </p>
+			
 		</div>
+	</div>
     <section>
         <h3> What is Chalk it Up ? </h3>
 			<div class="well">
@@ -49,7 +44,7 @@
 				</div>
 				<div class="row">
 					<div class="col-lg-4">
-                        <p> Lorum Ipsum </p>
+                        <p> MORE TEXT  <img class="big-image" style="height:300px" src="https://www.googledrive.com/host/0B9YkvbHM062yZTJzdGtGUi1jelk/img/logo_horzontal.png"/> </p>
 					</div>
 					<div class="col-lg-4">
 						<p> Lorum Ipsum </p>
