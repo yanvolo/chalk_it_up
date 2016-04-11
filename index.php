1a96abec7d85f3bd59fb04b74daa630ef3d7562a
yan vologzhanin <yanvolo@gmail.com>
2016-04-11 16:56:10 -0400

    Updated Jason's Page

--- index.php
+++ index.php
@@ -28,52 +28,120 @@ needUserInfo();
 			
 		</div>
 	</div>
-    <section>
-        <h3> What is Chalk it Up ? </h3>
+        <section>
 			<div class="well">
-				<div class="page-header" id="feedback">
-					<h2> Feedback <small> This is feedback </small> </h2>
-				</div>
-				<div class="row">
-					<div class="col-lg-4">
-                        <p> MORE TEXT  <img class="big-image" style="height:300px" src="https://www.googledrive.com/host/0B9YkvbHM062yZTJzdGtGUi1jelk/img/logo_horzontal.png"/> </p>
-					</div>
-					<div class="col-lg-4">
-						<p> Lorum Ipsum </p>
-					</div>
-					<div class="col-lg-4">
-						<p> Lorum Ipsum </p>
-					</div>
+				<div class="page-header" id="summary">
+				    <h1> <center> What is ChalkItUp? </center> </h1>
+                	<center> <div class="row">
+            	<div class="col-lg-4">
+            	    <h3> Education </h3>
+                    <h4> ChalkItUp is an educational tool to help teach students the knowledge they need to succeed. </h4>
+     			</div>
+     			<div class="col-lg-4">
+     				<h3> Teamwork </h3>
+      				<h4> Students work together in classrooms to defeat "enemies" and reach the goal of gaining knowledge. </h4>
+      			<img class="big-image" src="https://www.googledrive.com/host/0B9YkvbHM062yZTJzdGtGUi1jelk/img/bosses/chemisludge_digital.jpg"/>
+     			</div>
+     			<div class="col-lg-4">
+     				<h3> Fun </h3>
+      				<h4> Entertaining visuals and trivia create an enjoyable experience that keeps students focused. </h4>
+      			<img class="big-image" src="https://www.googledrive.com/host/0B9YkvbHM062yZTJzdGtGUi1jelk/img/UI.png"/>
+     			</div>
+    		</div> <center>
 				</div>
 			</div>
-		</section>
-		<h2> Chalk it Image Library </h2>
-		<p> Images with sources for web devs </p>
-		<figure>
-			<img class="big-image" src="https://www.googledrive.com/host/0B9YkvbHM062yZTJzdGtGUi1jelk/img/logo_horzontal.png"/>
-			<figcaption> logo_horzontal.png </figcaption>
-		</figure>
-		<figure>
-			<img class="big-image" src="https://www.googledrive.com/host/0B9YkvbHM062yZTJzdGtGUi1jelk/img/logo_vertical.png"/>
-			<figcaption> logo_vertical.png </figcaption>
-		</figure>
-		<figure>
+			//delete this section when finished
+		<div class="row">
+   			<div class="col-lg-6">
+ 		<figure>
+		    <h3> SQRT-1 (Mathematics) </h3>
 			<img class="big-image" src="https://www.googledrive.com/host/0B9YkvbHM062yZTJzdGtGUi1jelk/img/bosses/sqrt-1_sketch.jpg"/>
-			<figcaption> /bosses/sqrt-1_sketch.jpg </figcaption>
 		</figure>
 		<figure>
+			<h3> Grock (Social Studies) </h3>
 			<img class="big-image" src="https://www.googledrive.com/host/0B9YkvbHM062yZTJzdGtGUi1jelk/img/bosses/grock_sketch.jpg"/>
-			<figcaption> /bosses/grock_sketch.jpg </figcaption>
+		</figure>
+     		</div>
+     		<div class="col-lg-6">
+      	<figure>
+			<h3> Chemisludge (Science) </h3>
+			<img class="big-image" src="https://www.googledrive.com/host/0B9YkvbHM062yZTJzdGtGUi1jelk/img/bosses/chemisludge_digital.jpg"/>
 		</figure>
 		<figure>
-			<img class="big-image" src="https://www.googledrive.com/host/0B9YkvbHM062yZTJzdGtGUi1jelk/img/bosses/chemisludge_sketch.jpg"/>
-			<figcaption> /bosses/chemisludge_sketch.jpg </figcaption>
-		</figure>
-		<figure>
+			<h3> Bookworm (Language Arts) </h3>
 			<img class="big-image" src="https://www.googledrive.com/host/0B9YkvbHM062yZTJzdGtGUi1jelk/img/bosses/bookworm_sketch.jpg"/>
-			<figcaption> /bosses/bookworm_sketch.jpg </figcaption>
-
 		</figure>
+		<div class="container">
+			<container>
+				<div class="page-header" id="features">
+					<h2> Features <small> This is still not feedback </small> </h2>
+				</div>
+
+				<div class="row">
+					<div class="col-lg-8">
+						<h3> This is the heading </h3>
+						<p> More lorum ipsum text that does not matter </p>
+					</div>
+					<div class="col-lg-4">
+						<img src="img/imac.jpg" class="img-responsive" alt="image">
+					</div>
+				</div>
+
+				<div class="row">
+					<div class="col-lg-8">
+						<h3> This is the heading </h3>
+						<p> More lorum ipsum text that does not matter </p>
+					</div>
+					<div class="col-lg-4">
+						<img src="img/smartphone.jpg" class="img-responsive" alt="image">
+					</div>
+				</div>
+
+				<div class="row">
+					<div class="col-lg-8">
+						<h3> This is the heading </h3>
+						<p> More lorum ipsum text that does not matter </p>
+					</div>
+					<div class="col-lg-4">
+						<img src="img/user.jpg" class="img-responsive" alt="image">
+					</div>
+				</div>
+
+				<hr>
+
+				<div class="row">
+					<div class="col-lg-4">
+							<div class="panel panel-default">
+								<div class="panel-body text-center">
+									<span class="glyphicon glyphicon-info-sign"> </span>
+									<h4> Sure</h4>
+									<p> This is more content</h4>
+								</div>
+							</div>
+					</div>
+
+					<div class="col-lg-4">
+							<div class="panel panel-default">
+								<div class="panel-body text-center">
+									<h4> Sure</h4>
+									<p> This is more content</h4>
+								</div>
+							</div>
+					</div>
+
+					<div class="col-lg-4">
+							<div class="panel panel-default">
+								<div class="panel-body text-center">
+									<h4> Sure</h4>
+									<p> This is more content</h4>
+								</div>
+							</div>
+					</div>
+
+
+				</div>
+			</container>
+		</div>
 
   </body>
 
