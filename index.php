cb8ef25bfa13b14fb12d75fde166b7dd71acb08a
yan vologzhanin <yanvolo@gmail.com>
2016-04-11 22:45:29 -0400

    Added all sizes to col's

--- index.php
+++ index.php
@@ -59,17 +59,17 @@ needUserInfo();
 				<div class="page-header" id="summary">
 				    <h1> <center> What is ChalkItUp? </center> </h1>
                 	<center> <div class="row">
-            	<div class="col-lg-4">
+            	<div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
             	    <h3> Education </h3>
                     <h4> ChalkItUp is an educational tool to help teach students the knowledge they need to succeed. </h4>
                      <img class="small-image" src="https://www.googledrive.com/host/0B9YkvbHM062yZTJzdGtGUi1jelk/img/bosses/sqrt-1_sketch.jpg"/>
      			</div>
-     			<div class="col-lg-4">
+     			<div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
      				<h3> Teamwork </h3>
       				<h4> Students work together in classrooms to defeat "enemies" and reach the goal of gaining knowledge. </h4>
       			<img class="big-image" src="https://www.googledrive.com/host/0B9YkvbHM062yZTJzdGtGUi1jelk/img/bosses/chemisludge_digital.jpg"/>
      			</div>
-     			<div class="col-lg-4">
+     			<div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
      				<h3> Fun </h3>
       				<h4> Entertaining visuals and trivia create an enjoyable experience that keeps students focused. </h4>
       			<img class="big-image" src="https://www.googledrive.com/host/0B9YkvbHM062yZTJzdGtGUi1jelk/img/UI.png"/>
@@ -86,30 +86,30 @@ needUserInfo();
 	</div>
 	<div class="container">
 				<div class="row">
-					<div class="col-lg-8">
+					<div class="col-lg-8 col-md-8 col-sm-8 col-xs-8">
 						<h3> IT Expo at UC (4/12/16) </h3>
 						<p> Walnut Hills INTERalliance presentation of ChalkItUp at University of Cincinnati at 10 AM. Be there by 8:30 AM. </p>
 					</div>
-					<div class="col-lg-4">
+					<div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
 					<center> <img class="news-image" src="https://www.googledrive.com/host/0B9YkvbHM062yZTJzdGtGUi1jelk/img/uc.png" /> </center>
 					</div>
 				</div>
 				<div class="row">
-					<div class="col-lg-8">
+					<div class="col-lg-8 col-md-8 col-sm-8 col-xs-8">
 						<h3> INTERalliance Meeting (4/14/16) </h3>
 						<p> INTERalliance meeting after school to discuss information technology and business, as well as future plans for the club. </p>
 					</div>
-					<div class="col-lg-4">
+					<div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
 					<center> <img class="news-image" src="https://www.googledrive.com/host/0B9YkvbHM062yZTJzdGtGUi1jelk/img/interalliance.jpg" /> </center>
 					</div>
 				</div>
 
 				<div class="row">
-					<div class="col-lg-8">
+					<div class="col-lg-8 col-md-8 col-sm-8 col-xs-8">
 						<h3> Gaming Day (4/21/16) </h3>
 						<p> INTERalliance will host a gaming day, where we will play and discuss videogames and their impact in the IT field. </p>
 					</div>
-					<div class="col-lg-4">
+					<div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
 					<center> <img class="news-image" src="https://www.googledrive.com/host/0B9YkvbHM062yZTJzdGtGUi1jelk/img/game.png" /> </center>
 					</div>
 				</div>
