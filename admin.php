19c14f7f61acfbed4c29c62e9eaaa798ba035c95
yan vologzhanin <yanvolo@gmail.com>
2016-04-11 22:01:51 -0400

    Formated Admin Page

--- admin.php
+++ admin.php
@@ -13,26 +13,26 @@ if(!$is_admin){
 </head>
 <body>
 <?php printNav();?>
-
+<div class="container">
 <h1>Admin</h!>
                                                      <h2>Add class</h2>
                                                      <form action='new_class.php' method='POST'>
-                                                     <input type='text' name='display_name' placeholder="Display Name class(display_name)"/><br/>
+                                                     <input class="form-control" type='text' name='display_name' placeholder="Display Name class(display_name)"/><br/>
                                                      <?php $select_teacher_modal = userSelectionInput('teachers', 'Teachers');?><br/>
-                                                     <input type='submit' value='Create'/><br/>
+                                                     <input class="form-control" type='submit' value='Create'/><br/>
                                                      </form>
 <?php echo $select_teacher_modal;?>
 
                                                      <h2>Add user</h2>
                                                      <form action='new_user.php' method='POST'>
-                                                     <input type='text' name='login_name' placeholder="Login Name"/><br/>
-                                                     <input type='text' name='display_name' placeholder="Display Name"/><br/>
-                                                     <input type='password' name='password' placeholder="(optional) Default Password"/><br/>
-                                                     <input type='submit' value='Create'/><br/>
+                                                     <input class="form-control" type='text' name='login_name' placeholder="Login Name"/><br/>
+                                                     <input class="form-control" type='text' name='display_name' placeholder="Display Name"/><br/>
+                                                     <input class="form-control" type='password' name='password' placeholder="(optional) Default Password"/><br/>
+                                                     <input class="form-control" type='submit' value='Create'/><br/>
                                                      </form>
 
 
-
+</div>
 </body>
                                                      <html>
                                                      
